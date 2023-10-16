4d21f19be0998a870bec0904d69b54ba15486f3f
vyshnaviK18 <113926615+vyshnaviK18@users.noreply.github.com>
2023-10-16 12:34:08 +0530

    Update import nltk.py

--- import nltk.py
+++ import nltk.py
@@ -15,21 +15,11 @@ txt = "Sukanya, Rajib and Naba are my good friends. " \
 	"Many of you must have tried searching for a friend "\
 	"but never found the right one."
 
-# sent_tokenize is one of instances of 
-# PunktSentenceTokenizer from the nltk.tokenize.punkt module
-
 tokenized = sent_tokenize(txt)
 for i in tokenized:
-	
-	# Word tokenizers is used to find the words 
-	# and punctuation in a string
+
 	wordsList = nltk.word_tokenize(i)
-
-	# removing stop words from wordList
 	wordsList = [w for w in wordsList if not w in stop_words] 
-
-	# Using a Tagger. Which is part-of-speech 
-	# tagger or POS-tagger. 
 	tagged = nltk.pos_tag(wordsList)
 
 	print(tagged)
