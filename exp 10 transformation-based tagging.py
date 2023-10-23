c86851e30dd3b373afa9cea0f556c3af32ddcc13
vyshnaviK18 <113926615+vyshnaviK18@users.noreply.github.com>
2023-10-23 22:13:33 +0530

    Update exp 10 transformation-based tagging.py

--- exp 10 transformation-based tagging.py
+++ exp 10 transformation-based tagging.py
@@ -1,20 +1,9 @@
 def transformation_based_tagging(text):
     words = text.split()
-    tagged_words = []
-    
-    for i in range(len(words)):
-        word = words[i]
-
-        if word[0].isupper() and i != 0:
-            tagged_word = (word, 'NNP')
-        else:
-            tagged_word = (word, 'NN')
-        
-        tagged_words.append(tagged_word)
-    
+    tagged_words = [(word, 'NNP' if word[0].isupper() else 'NN') for word in words]
     return tagged_words
 
-#main
+# Main
 text = "Alice lives in Wonderland. She loves it there."
 tagged_words = transformation_based_tagging(text)
 print(tagged_words)
