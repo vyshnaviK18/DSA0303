719932a529fc03b612e67c1dce7235305bbf5117
vyshnaviK18 <113926615+vyshnaviK18@users.noreply.github.com>
2023-10-23 21:38:25 +0530

    Update exp 7parts of speech.py

--- exp 7parts of speech.py
+++ exp 7parts of speech.py
@@ -1,7 +1,7 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-stop_words = set(stopwords.words('english'))
+
 
 txt = "The quick brown fox jumps over the lazy dog. " \
       "Python is a versatile programming language. " \
@@ -13,10 +13,6 @@ tokenized = sent_tokenize(txt)
 for i in tokenized:
 	
 	wordsList = nltk.word_tokenize(i)
-
-	# removing stop words 
-	wordsList = [w for w in wordsList if not w in stop_words] 
-
 	# POS-tagger. 
 	tagged = nltk.pos_tag(wordsList)
 
